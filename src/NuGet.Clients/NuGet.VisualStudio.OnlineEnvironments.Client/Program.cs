73761cf4a58b7c2246d5cd839fb830c904ac06ba
Nikolche Kolev <nikolev@microsoft.com>
2020-02-26 22:50:04 -0800

    work

---
+++ src/NuGet.Clients/NuGet.VisualStudio.OnlineEnvironments.Client/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NuGet.VisualStudio.OnlineEnvironments.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
